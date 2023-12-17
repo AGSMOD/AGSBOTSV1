41d5e4803e727a548465220cf43ebd526fb9e4f7
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-12-17 19:09:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,8 @@ def is_enabled(value, default):
     else: return default
 
 
-# PyroClient Setup 
+# PyroClient Setup
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
